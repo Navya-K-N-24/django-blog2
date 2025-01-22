f26b78e49080ef0ae7274b0430b044bc2d3c15c2
Navya Keelara <navyakn31@gmail.com>
2025-01-22 12:16:52 +0000

    update urls

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-4g24ttlp*q(*7z1ged!wts3@zm!y8_d@od%=ojk6q-ihk3rrn)
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-navyakn24-djangoblog2-ls0el4vmjw6.ws.codeinstitute-ide.net','.herokuapp.com',]
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
